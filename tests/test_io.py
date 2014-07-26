b1c943aa18daab11be01274c7d52ada6de53df7f
JuanPablo <jpabloaj@gmail.com>
2014-07-26 20:25:16 +0000

    file_sha1sum test

--- tests/test_io.py
+++ tests/test_io.py
@@ -5,24 +5,37 @@ import tempfile
 import os
 
 from gitdata import get_file_list
+from gitdata import file_sha1sum
 
 class TestIO(unittest.TestCase):
 
     @classmethod
     def setUpClass(cls):
         cls.tempdir = tempfile.mkdtemp()
-
-    def test_list_files(self):
-        dir_name = 'files'
-        files_dir = os.path.join(self.tempdir, dir_name)
-        os.mkdir(files_dir)
-        os.chdir(self.tempdir)
+        cls.dir_name = 'files'
+        cls.files_dir = os.path.join(cls.tempdir, cls.dir_name)
+        os.mkdir(cls.files_dir)
 
         new_files = ['file1.txt', 'file2.txt']
 
         for fname in new_files:
-            open(os.path.join(files_dir, fname), 'w')
+            open(os.path.join(cls.files_dir, fname), 'w').write(fname)
+
+    def test_list_files(self):
+        os.chdir(self.tempdir)
 
         paths = ['files/file1.txt', 'files/file2.txt']
 
-        self.assertEqual(sorted(get_file_list(dir_name)), sorted(paths))
+        self.assertEqual(sorted(get_file_list(self.dir_name)), sorted(paths))
+
+    def test_file_sha1sum(self):
+
+        file_path = os.path.join(self.files_dir, 'file1.txt')
+
+        self.assertEqual(file_sha1sum(file_path), 'ce1be0ff4065a6e9415095c95f25f47a633cef2b')
+
+    def test_file_sha1sum_file_not_found(self):
+
+        file_path = os.path.join(self.files_dir, 'fail_file.txt')
+
+        self.assertEqual(file_sha1sum(file_path), None)
