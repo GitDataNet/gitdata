3f008361b14cbb6b26629fcd17fae77a61346285
JuanPablo <jpabloaj@gmail.com>
2014-08-03 01:31:23 +0000

    gitdata_readlines test added

--- tests/test_git.py
+++ tests/test_git.py
@@ -7,6 +7,7 @@ import subprocess
 
 from gitdata.git import git_root
 from gitdata import gitdata_path
+from gitdata import gitdata_readlines
 
 
 class TestGit(unittest.TestCase):
@@ -28,3 +29,29 @@ class TestGit(unittest.TestCase):
         path = '{}/{}'.format(self.tempdir, '.gitdata')
 
         self.assertEqual(gitdata_path(), path)
+
+    def test_gitdata_readlines_no_file(self):
+
+        self.assertEqual(gitdata_readlines(), [])
+
+
+class TestGitdata(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.tempdir = tempfile.mkdtemp()
+        os.chdir(cls.tempdir)
+        subprocess.check_output(['git', 'init'])
+
+    def setUp(self):
+        os.chdir(self.tempdir)
+
+    def tearDown(self):
+        os.remove('.gitdata')
+
+    def test_gitdata_readlines(self):
+
+        open('.gitdata', 'w').write('aaa\nbbb')
+        content = ['aaa\n', 'bbb']
+
+        self.assertEqual(gitdata_readlines(), content)
